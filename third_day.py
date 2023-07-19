5189c6c84af2e37ecb37750401b479838bb8c866
JakmanLoss <74964584+JakmanLoss@users.noreply.github.com>
2023-07-19 16:41:14 +0300

    Add files via upload

---
+++ third_day.py
@@ -0,0 +1,41 @@
+f = open('pr.txt')
+# school_class = [int(x) for x in f.readlines()] #считывание файла в список с помощью генератора списков
+school_class = []
+for x in f.readlines():
+    school_class.append(int(x))  # альтернативное считывание, делаем int, чтобы убрать знак переноса
+print(school_class)
+ch_poz = 10**10
+nc_poz = 10**10
+for i in range(0, len(school_class), 2):
+    if school_class[i] % 2 != 0:
+        ch_poz = min(ch_poz, school_class[i])#нахождение минимального нечетного на чет поз
+
+for j in range(1, len(school_class), 2):
+    if school_class[j] % 2 == 0:
+        nc_poz = min(nc_poz, school_class[j])#нахождение минимального четного на неч поз
+kc = 0
+kn = 0
+for l in range(2, len(school_class), 2):
+    if (school_class[l] % ch_poz == 0 and school_class[l - 2] % ch_poz != 0) or (school_class[l - 2] % ch_poz == 0 and school_class[l] % ch_poz != 0):
+        kc += 1
+#нахождение напрямую
+masch = []
+for i in range(len(school_class)):
+    if i % 2 == 0:
+        masch.append(school_class[i])#добавляем в массив все элементы на чётной позиции
+
+'''for i in range(len(school_class) - 1):
+    if (school_class[i] % ch_poz == 0 and school_class[i + 1] % ch_poz != 0) or (school_class[i + 1] % ch_poz == 0 and school_class[i] % ch_poz != 0):
+        kc += 1'''
+#альтернативное нахождение пар
+
+
+for l in range(3, len(school_class), 2):
+    if (school_class[l] % nc_poz == 0 and school_class[l - 2] % nc_poz != 0) or (school_class[l - 2] % nc_poz == 0 and school_class[l] % nc_poz != 0):
+        kn += 1
+
+'''for i in range(len(school_class) - 1):
+    if (school_class[i] % nc_poz == 0 and school_class[i + 1] % nc_poz != 0) or (school_class[i + 1] % nc_poz == 0 and school_class[i] % nc_poz != 0):
+        kc += 1'''
+#альтернативное нахождение пар
+print(kc, kn)
